a752229301cd9840d059ae4016c67b157553a9db
Kwang_Ho Park <144115918+doncici77@users.noreply.github.com>
2025-02-09 12:30:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        char input = Console.ReadLine()[0]; // 한 줄을 입력받고 첫 번째 문자 사용
+        Console.WriteLine((int)input); // ASCII 코드 출력
+    }
+}
