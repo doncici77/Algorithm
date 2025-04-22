b824de0cdc3231f8a41d505926b18159df09990a
Kwang_Ho Park <144115918+doncici77@users.noreply.github.com>
2025-04-22 10:03:02 +0900

    [Silver I] Title: 숨바꼭질, Time: 60 ms, Memory: 10540 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cs
@@ -0,0 +1,48 @@
+class BJ1697
+{
+    static int[] map = new int[100_007]; // 맵
+    static int N; // 언니
+    static int K; // 동생
+
+    static Queue<int> queue = new Queue<int>();
+    static bool[] isVisited = new bool[100_007];
+    static int[] wayDistance = new int[100_007];
+
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        N = int.Parse(input[0]);
+        K = int.Parse(input[1]);
+
+        queue.Enqueue(N);
+        wayDistance[N] = 0;
+        isVisited[N] = true;
+
+        while (queue.Count > 0)
+        {
+            int currentPos = queue.Dequeue();
+
+            int[] move = { +1, -1, +currentPos };
+
+            if (currentPos == K)
+            {
+                break;
+            }
+
+            for(int i = 0; i < move.Length; i++)
+            {
+                if (currentPos + move[i] >= 0 && currentPos + move[i] < 100_007)
+                {
+                    if (isVisited[currentPos + move[i]] == false)
+                    {
+                        isVisited[currentPos + move[i]] = true;
+                        queue.Enqueue((currentPos + move[i]));
+                        wayDistance[(currentPos + move[i])] = wayDistance[currentPos] + 1;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(wayDistance[K].ToString());
+    }
+}
